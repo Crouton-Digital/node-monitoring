269f41361137b59911cbfdc1d21000c9a7f5a0c9
thousandsofthem <thousandsofthem@gmail.com>
2023-02-01 10:17:50 +0100

    generate config dynamically

--- backend/internal/trafik_config/trafik_config.go
+++ backend/internal/trafik_config/trafik_config.go
@@ -3,6 +3,7 @@ package trafik_config
 import (
 	"log"
 	"net/http"
+	"node-balancer/internal/server/config"
 
 	yaml3 "gopkg.in/yaml.v3"
 )
@@ -28,9 +29,9 @@ type Server struct {
 	URL string `yaml:"url"`
 }
 type LoadBalancer struct {
-	HealthCheck    HealthCheck `yaml:"healthCheck"`
-	Servers        []Server    `yaml:"servers"`
-	PassHostHeader bool        `yaml:"passHostHeader"`
+	// HealthCheck    HealthCheck `yaml:"healthCheck"`
+	Servers        []Server `yaml:"servers"`
+	PassHostHeader bool     `yaml:"passHostHeader"`
 }
 
 type Service struct {
@@ -44,61 +45,62 @@ type HTTP struct {
 
 func GenerateConfig() TrafikConfig {
 
-	config := TrafikConfig{
-		HTTP: HTTP{
-			Routers: map[string]Router{
-				"polygon_node": {
-					EntryPoints: []string{"web"},
-					Service:     "polygon_node",
-					Rule:        "Host(`polygon-nodes.dex-arbitrage.svc.cluster.local`)",
-				},
-				"polygon_node_ws": {
-					EntryPoints: []string{"ws"},
-					Service:     "polygon_node_ws",
-					Rule:        "Host(`polygon-nodes.dex-arbitrage.svc.cluster.local`)",
-				},
-			},
-			Services: map[string]Service{
-				"polygon_node": {
-					LoadBalancer: LoadBalancer{
-						HealthCheck: HealthCheck{
-							Path: "/",
-							Port: 36360,
-							Headers: map[string]string{
-								"Content-Type": "application/json",
-							},
-							Interval: "10s",
-							Timeout:  "3s",
-						},
-						Servers: []Server{
-							{URL: "http://144.76.18.142:36360"},
-							{URL: "http://65.108.201.189:36360"},
-						},
-					},
-				},
-				"polygon_node_ws": {
-					LoadBalancer: LoadBalancer{
-						HealthCheck: HealthCheck{
-							Path: "/",
-							Port: 36360,
-							Headers: map[string]string{
-								"Content-Type": "application/json",
-							},
-							Interval: "10s",
-							Timeout:  "3s",
-						},
-						Servers: []Server{
-							{URL: "http://144.76.18.142:36361"},
-							{URL: "http://65.108.201.189:36361"},
-						},
-						PassHostHeader: true,
-					},
-				},
-			},
-		},
+	tfConfig := TrafikConfig{HTTP: HTTP{
+		Routers:  map[string]Router{},
+		Services: map[string]Service{},
+	}}
+
+	for network, domain := range config.Config.DomainsConfig {
+		tfConfig.HTTP.Routers[network+"_node"] = Router{
+			EntryPoints: []string{"web"},
+			Service:     network + "_node",
+			Rule:        "Host(`" + domain.Url + "`)",
+		}
+		tfConfig.HTTP.Routers[network+"_node_ws"] = Router{
+			EntryPoints: []string{"ws"},
+			Service:     network + "_node_ws",
+			Rule:        "Host(`" + domain.Url + "`)",
+		}
 	}
 
-	return config
+	for network, nodes := range config.Config.Nodes {
+		httpServers := []Server{}
+		wsServers := []Server{}
+		for _, node := range nodes {
+			ok := true
+			if node.AllowRouting && ok {
+				httpServers = append(httpServers, Server{URL: node.Url})
+
+				if node.WsSupport {
+					wsServers = append(wsServers, Server{URL: node.Url})
+				}
+			}
+		}
+
+		tfConfig.HTTP.Services[network+"_node"] = Service{
+			LoadBalancer: LoadBalancer{
+				// HealthCheck: HealthCheck{
+				// 	Path: "/",
+				// 	Port: 36360,
+				// 	Headers: map[string]string{
+				// 		"Content-Type": "application/json",
+				// 	},
+				// 	Interval: "10s",
+				// 	Timeout:  "3s",
+				// },
+				Servers:        httpServers,
+				PassHostHeader: true,
+			},
+		}
+		tfConfig.HTTP.Services[network+"_node_ws"] = Service{
+			LoadBalancer: LoadBalancer{
+				Servers:        wsServers,
+				PassHostHeader: true,
+			},
+		}
+	}
+
+	return tfConfig
 }
 
 func HandleConfig(w http.ResponseWriter, r *http.Request) {
